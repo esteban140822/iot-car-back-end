2bcc371c9ad41cc09f9e82c8b6f1f2ada5cd477b
esteban140822 <estebanbarreola@gmail.com>
2024-05-24 15:33:41 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -4,7 +4,7 @@ let respuesta = document.getElementById('respuesta');
 function callApiRequest(){
 
     // Hacer una petición para un usuario con ID especifico
-axios.get('https://3.143.245.144/iot-car-control/back-end/apis/getRegistro.php')
+axios.get('https://3.139.64.70/iot-car-control/back-end/apis/getRegistro.php')
 .then(function (response) {
   // manejar respuesta exitosa
   console.log(response);
